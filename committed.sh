3bf7bcc342658c87341231892703f7373ed9e51b
Mike Elliot <melliott@nav.com>
2017-11-08 20:06:15 -0700

    Love is never asking why?

--- committed.sh
+++ committed.sh
@@ -4,7 +4,9 @@ NUMBER=$(($RANDOM % 30))
 
 for run in {1...$NUMBER}
 do
-  sh ./fortunate.sh && git add . -A && git commit -m "$FORTUNE"
+  sh ./fortunate.sh
+  git add . -A
+  git commit -m "$FORTUNE"
 done
 
 
