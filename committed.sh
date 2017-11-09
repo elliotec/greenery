2739b13ba565cc0d24942b7056a74faf143f3adf
Mike Elliot <melliott@nav.com>
2017-11-08 20:20:26 -0700

    Your lucky color has faded.

--- committed.sh
+++ committed.sh
@@ -3,9 +3,7 @@ FORTUNE=`fortune -sn 30`
 
 for run in {1..$(($RANDOM % 30))}
 do
-  sh ./fortunate.sh
-  git add . -A
-  git commit -m "$FORTUNE"
+  sh ./fortunate.sh && git add . -A && git commit -m "$FORTUNE"
 done
 
 
