3300dde8c1e022987f1b323aa0d87095f662d50e
elliotec <elliotecweb@gmail.com>
2017-11-08 23:52:19 -0700

    move it

--- committed.sh
+++ committed.sh
@@ -3,8 +3,8 @@ NUMBER=$(($RANDOM % 23))
 
 for ((run=1; run <= NUMBER; run++))
 do
-  sh ~/Dropbox/code/pusher/fortunate.sh
-  /usr/bin/git -C ~/Dropbox/code/pusher/ add . -A
-  /usr/bin/git -C ~/Dropbox/code/pusher/ commit -m "`/usr/local/bin/fortune -sn 32`"
+  sh ~/pusher/fortunate.sh
+  /usr/bin/git -C ~/pusher/ add . -A
+  /usr/bin/git -C ~/pusher/ commit -m "`/usr/local/bin/fortune -sn 32`"
 done
 
