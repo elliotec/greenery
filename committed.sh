a77c20037b126a5d7c5ce7c7279c1ad5ace0c61a
Mike Elliot <melliott@nav.com>
2017-11-08 20:26:52 -0700

    Who's on first?

--- committed.sh
+++ committed.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-FORTUNE=`fortune -sn 30`
-RANGE=30
-NUMBER=$(($RANDOM % $RANGE))
+NUMBER=$(($RANDOM % 30))
 
 for ((run=1; run <= NUMBER; run++))
 do
   sh ./fortunate.sh
   git add . -A
-  git commit -m "$FORTUNE"
+  git commit -m "`fortune -sn 30`"
 done
 
