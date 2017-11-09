996e276b6407877d411c6144203d600fdf75712a
Mike Elliot <elliotecweb@gmail.com>
2017-11-08 21:02:52 -0700

    You now have Asian Flu.

--- committed.sh
+++ committed.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-NUMBER=$(($RANDOM % 30))
+NUMBER=$(($RANDOM % 23))
 
 for ((run=1; run <= NUMBER; run++))
 do
   cd ~/Dropbox/code/pusher
   sh ~/Dropbox/code/pusher/fortunate.sh
   git add . -A
-  git commit -m "`fortune -sn 30`"
+  git commit -m "`fortune -sn 32`"
 done
 
